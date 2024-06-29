c683118c5ad67423667ba1bc90feb52edd93ed6f
Ryan Witzman <rwitzman4@gmail.com>
2024-06-29 12:02:08 -0400

    Implemented new menu and game initialization logic, removed unused code.

--- public/main.js
+++ public/main.js
@@ -1,57 +1,32 @@
-import { updateParagraphDisplay, handleUserInput, getProgress, setSocket, initUI } from './ui.js';
-import { initThreeJS, animate, createPlayerCar, updateCarProgress } from './threeSetup.js';
-import { initCutScene } from './cutScene.js';
+import { initMenu } from './menu.js';
+import { initGame } from './game.js';
 
 const socket = io();
-setSocket(socket);
+window.socket = socket;
 
-let players = new Map();
-let cars = new Map();
+let currentState = 'menu';
 
-initThreeJS();
-animate();
-initUI();
-initCutScene();
+initMenu();
 
-function createOrUpdateCar(playerId) {
-    if (!cars.has(playerId)) {
-        createPlayerCar(playerId);
-    }
-    updateCarProgress(playerId, players.get(playerId).progress);
+socket.on('lobbyCreated', (lobbyId) => {
+    console.log(`Lobby created with ID: ${lobbyId}`);
+    // Implement lobby waiting room
+});
+
+socket.on('lobbyList', (lobbies) => {
+    showLobbyList(lobbies);
+});
+
+socket.on('joinedLobby', (lobbyId) => {
+    console.log(`Joined lobby with ID: ${lobbyId}`);
+    // Implement lobby waiting room
+});
+
+socket.on('gameStarted', (gameState) => {
+    currentState = 'game';
+    initGame(gameState);
+});
+
+export function startGame() {
+    socket.emit('startGame');
 }
-
-socket.on('gameState', ({ players: serverPlayers, paragraph }) => {
-    players = new Map(serverPlayers.map(p => [p.id, p]));
-    updateParagraphDisplay(paragraph);
-    players.forEach((player, id) => createOrUpdateCar(id));
-});
-
-socket.on('newRound', ({ paragraph, players: serverPlayers }) => {
-    players = new Map(serverPlayers.map(p => [p.id, p]));
-    updateParagraphDisplay(paragraph);
-    players.forEach((player, id) => createOrUpdateCar(id));
-});
-
-socket.on('playerProgress', ({ id, progress }) => {
-    if (players.has(id)) {
-        players.get(id).progress = progress;
-        updateCarProgress(id, progress);
-    }
-});
-
-socket.on('playerDisconnected', (id) => {
-    players.delete(id);
-});
-
-document.addEventListener('keydown', (event) => {
-    handleUserInput(event);
-    const progress = getProgress();
-    socket.emit('typingProgress', progress);
-    updateCarProgress(socket.id, progress);
-});
-
-function updatePlayers(serverPlayers) {
-    players = new Map(serverPlayers.map(p => [p.id, p]));
-}
-
-export { players };
